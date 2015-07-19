e3a9cf0edc51d07da851e4bf133af33ffc443c59
Thordur Thordarson <thor@runesmith.org>
2015-07-18 20:34:15 -0400

    Added WPF project which will hopefully replace the Windows Forms Project

---
+++ FMDraft.WPF/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using FMDraft.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FMDraft.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GameCore game = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        public bool IsGameLoaded
+        {
+            get
+            {
+                return game != null && game.IsLoaded;
+            }
+        }
+
+        private void InitializeGame()
+        {
+            game = new GameCore();
+
+            game.LoadFailedCallback += () =>
+            {
+                MessageBox.Show("Could not load data. Ensure that Football Manager 2015 is running and that no Firewall is prevent this application from accessing it.");
+            };
+
+            game.LoadCompleteCallback += () =>
+            {
+                MessageBox.Show("Data has been loaded");
+            };
+        }
+
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            game.Load();
+        }
+
+        private void Quit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ConfederationTab_Selected(object sender, RoutedEventArgs e)
+        {
+            if (IsGameLoaded)
+            {
+                PrincipalNationsComboBox.ItemsSource = game.QueryService.GetNations();
+            }
+        }
+    }
+}
